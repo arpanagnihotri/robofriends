8e2b08d8fc6dc12b680ff3d56aa6df7ca92f8085
agniarpan2209 <babaeyega1812710011@gmail.com>
2020-08-11 14:35:11 +0530

    Updates

---
+++ precache-manifest.476fa2d301c3fb118734486fe1bbcd53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1db32505e36d36fa9984c957b1f6aed",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ee9adf9ed1bf37b7cece",
+    "url": "/robofriends/static/css/main.0e9e3734.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee9adf9ed1bf37b7cece",
+    "url": "/robofriends/static/js/main.4061a24f.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
